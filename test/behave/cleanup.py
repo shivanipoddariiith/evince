145d5690794fc315f06fda8176c1375f45a1ec76
Shivani Poddar <shivani.poddar92@gmail.com>
2014-08-19 01:24:22 +0530

    Revert "Adding tests for Evince"
    This reverts commit 3c2690b7e7b85e475b0e84348c087689e8881bc5.

--- test/behave/cleanup.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python
-# This cleans existing gnome-weather data
-
-import os
-from gi.repository import Gio
-
-# Reset GSettings
-for schema in ['org.gnome.Evince']:
-    os.system("gsettings reset-recursively %s" % schema)
